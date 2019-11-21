5984652b83b7b350dceb8018144f1cf067a4662c
Sajin Thomas (UST, IND) <Sajin.Thomas@ust-global.com>
2019-11-21 18:00:13 +0530

    initial commit

---
+++ firstexp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace firstexp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
